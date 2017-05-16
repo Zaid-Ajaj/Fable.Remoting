55daa39f706931db9198ce2190dde318e0db0cee
Zaid-Ajaj <zaid.naom@gmail.com>
2017-05-16 22:23:09 +0200

    Publish Fable.Remoting.Reflection on dotnetcore2.0

---
+++ Fable.Remoting.Reflection.Core/FSharpRecord.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.CSharp;
+
+namespace Fable.Remoting.Reflection
+{
+    public static class FSharpRecord
+    {
+        static B Pipe<A, B>(this A x, Func<A, B> f) => f(x);
+
+        public static dynamic Invoke(string methodName, object implementation, object arg, bool hasArg)
+        {
+            return implementation
+                     .GetType()
+                     .GetProperty(methodName)
+                     .GetValue(implementation, null)
+                     .Pipe((dynamic fsFunc) => hasArg ? fsFunc.Invoke((dynamic)arg) : fsFunc.Invoke(null));
+        }
+    }
+}
