9efaeb51af3056f7c3389634f9de3a7e1b164fbe
Zaid <zaid.naom@gmail.com>
2017-12-23 04:51:15 +0100

    Remove redundant qualifiers from tests

--- Fable.Remoting.Suave.Tests/FableSuaveAdapterTests.fs
+++ Fable.Remoting.Suave.Tests/FableSuaveAdapterTests.fs
@@ -34,7 +34,7 @@ let ofJson<'t> (input: string) = JsonConvert.DeserializeObject<'t>(input, [| con
 let getConfig port = 
     { Suave.Web.defaultConfig 
         with bindings = [ HttpBinding.createSimple HTTP "127.0.0.1" port ] }
-let random = System.Random()
+let random = Random()
 let fableSuaveAdapterTests = 
     testList "FableSuaveAdapter tests" [
         testCase "Sending string as input works" <| fun () ->
@@ -42,14 +42,14 @@ let fableSuaveAdapterTests =
             let input = "\"my-test-string\"";
             let content = postContent input
             runWith defaultConfig app
-            |> req HttpMethod.POST "/IProtocol/getLength" (Some content)
+            |> req POST "/IProtocol/getLength" (Some content)
             |> fun result -> equal result "14"
 
         testCase "Sending int as input works" <| fun () ->
             let defaultConfig = getConfig (random.Next(1000, 9999))
             let input = postContent "5" 
             runWith defaultConfig app
-            |> req HttpMethod.POST "/IProtocol/echoInteger" (Some input)
+            |> req POST "/IProtocol/echoInteger" (Some input)
             |> fun result -> equal "10" result
 
     
@@ -58,7 +58,7 @@ let fableSuaveAdapterTests =
             let someInput = postContent "5" // toJson (Some 5) => "5"
             let testApp = runWith defaultConfig app
             testApp
-            |> req HttpMethod.POST "/IProtocol/echoOption" (Some someInput)
+            |> req POST "/IProtocol/echoOption" (Some someInput)
             |> fun result -> equal "10" result
 
         testCase "Sending none option as input works" <| fun () ->
@@ -69,7 +69,7 @@ let fableSuaveAdapterTests =
             let testApp = runWith defaultConfig app
             
             testApp
-            |> req HttpMethod.POST "/IProtocol/echoOption" (Some noneInput)
+            |> req POST "/IProtocol/echoOption" (Some noneInput)
             |> fun result -> equal "0" result
 
         
@@ -78,14 +78,14 @@ let fableSuaveAdapterTests =
             let someInput = postContent "\"2017-05-12T14:20:00.000Z\""
             let testApp = runWith defaultConfig app
             testApp
-            |> req HttpMethod.POST "/IProtocol/echoMonth" (Some someInput)
+            |> req POST "/IProtocol/echoMonth" (Some someInput)
             |> equal "5"
 
         testCase "Sending Result<int, string> roundtrip works with Ok" <| fun _ ->
             let defaultConfig = getConfig (random.Next(1000, 9999))
             let input = postContent (toJson (Ok 15))
             runWith defaultConfig app
-            |> req HttpMethod.POST "/IProtocol/echoResult" (Some input)
+            |> req POST "/IProtocol/echoResult" (Some input)
             |> ofJson<Result<int, string>>
             |> function 
                 | Ok 15 -> pass()
@@ -149,7 +149,7 @@ let fableSuaveAdapterTests =
             let someInput = postContent "{\"Just\":5}"
             let testApp = runWith defaultConfig app
             testApp
-            |> req HttpMethod.POST "/IProtocol/genericUnionInput" (Some someInput)
+            |> req POST "/IProtocol/genericUnionInput" (Some someInput)
             |> equal "5" 
             
         
@@ -158,7 +158,7 @@ let fableSuaveAdapterTests =
             let someInput = postContent "true"
             let testApp = runWith defaultConfig app
             testApp
-            |> req HttpMethod.POST "/IProtocol/genericUnionOutput" (Some someInput)
+            |> req POST "/IProtocol/genericUnionOutput" (Some someInput)
             |> equal "{\"Just\":5}"
 
         
@@ -167,7 +167,7 @@ let fableSuaveAdapterTests =
             let someInput = postContent "false"
             let testApp = runWith defaultConfig app
             testApp
-            |> req HttpMethod.POST "/IProtocol/genericUnionOutput" (Some someInput)
+            |> req POST "/IProtocol/genericUnionOutput" (Some someInput)
             |> equal "\"Nothing\""
 
         testCase "Recieving and sending simple union works" <| fun () -> 
@@ -175,7 +175,7 @@ let fableSuaveAdapterTests =
             let someInput = postContent "\"A\""
             let testApp = runWith defaultConfig app
             testApp
-            |> req HttpMethod.POST "/IProtocol/simpleUnionInputOutput" (Some someInput)
+            |> req POST "/IProtocol/simpleUnionInputOutput" (Some someInput)
             |> equal "\"B\""
             
         testCase "Recieving and sending records works" <| fun () -> 
@@ -185,7 +185,7 @@ let fableSuaveAdapterTests =
             let recordInput = postContent "{\"Prop1\":\"\",\"Prop2\":5,\"Prop3\":null}"
             let testApp = runWith defaultConfig app
             testApp
-            |> req HttpMethod.POST "/IProtocol/recordEcho" (Some recordInput)
+            |> req POST "/IProtocol/recordEcho" (Some recordInput)
             |> equal "{\"Prop1\":\"\",\"Prop2\":15,\"Prop3\":null}" 
 
         testCase "Sending list of ints works" <| fun () -> 
@@ -193,7 +193,7 @@ let fableSuaveAdapterTests =
             let someInput = postContent "[1,2,3,4,5,6,7,8,9,10]"
             let testApp = runWith defaultConfig app
             testApp
-            |> req HttpMethod.POST "/IProtocol/listIntegers" (Some someInput)
+            |> req POST "/IProtocol/listIntegers" (Some someInput)
             |> equal "55" 
 
         testCase "Inoking function of unit works" <| fun () -> 
@@ -202,7 +202,7 @@ let fableSuaveAdapterTests =
             let someInput = postContent ""
             let testApp = runWith defaultConfig app
             testApp
-            |> req HttpMethod.POST "/IProtocol/unitToInts" (Some someInput)
+            |> req POST "/IProtocol/unitToInts" (Some someInput)
             |> equal "55" 
 
         testCase "Invoking list of records works" <| fun () ->
@@ -210,7 +210,7 @@ let fableSuaveAdapterTests =
             let someInput = postContent "[{\"Prop1\":\"\",\"Prop2\":15,\"Prop3\":null}, {\"Prop1\":\"\",\"Prop2\":10,\"Prop3\":null}]"
             let testApp = runWith defaultConfig app
             testApp
-            |> req HttpMethod.POST "/IProtocol/recordListToInt" (Some someInput)
+            |> req POST "/IProtocol/recordListToInt" (Some someInput)
             |> equal "25" 
 
         testCase "Invoking a list of float works" <| fun () -> 
@@ -218,6 +218,6 @@ let fableSuaveAdapterTests =
             let someInput = postContent "[1.20, 1.40, 1.60]"
             let testApp = runWith defaultConfig app
             testApp
-            |> req HttpMethod.POST "/IProtocol/floatList" (Some someInput)
+            |> req POST "/IProtocol/floatList" (Some someInput)
             |> equal "4.2"
     ]
