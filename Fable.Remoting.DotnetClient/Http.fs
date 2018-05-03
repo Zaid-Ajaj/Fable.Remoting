7707f4b2b8810b6a912ac921e7e78f8b631f27de
Zaid <zaid.naom@gmail.com>
2018-05-03 10:48:16 +0200

    Dotnet client: Allow the use of an authorization header

--- Fable.Remoting.DotnetClient/Http.fs
+++ Fable.Remoting.DotnetClient/Http.fs
@@ -5,8 +5,12 @@ module Http =
     open System.Net
     open System.IO
 
+    type Authorisation = 
+        | Token of string
+        | NoToken 
+
     // From http://www.fssnip.net/7PK/title/Send-async-HTTP-POST-request
-    let makePostRequest (url : string) (requestBody : string) = 
+    let makePostRequest (url : string) (requestBody : string) auth = 
         let req = WebRequest.CreateHttp url
         req.CookieContainer <- new CookieContainer()
         req.Method <- "POST"
@@ -14,13 +18,17 @@ module Http =
         let postBytes = System.Text.Encoding.UTF8.GetBytes(requestBody)
         req.ContentLength <- postBytes.LongLength
         req.ContentType <- "application/json; charset=utf-8"
+        match auth with 
+        | Token authToken -> req.Headers.["Authorization"] <- authToken 
+        | NoToken -> () 
+
         async {
-            use! reqStream = req.GetRequestStreamAsync() |> Async.AwaitTask
-            do! reqStream.WriteAsync(postBytes, 0, postBytes.Length) |> Async.AwaitIAsyncResult |> Async.Ignore
+            use reqStream = req.GetRequestStream()
+            do reqStream.Write(postBytes, 0, postBytes.Length)
             reqStream.Close()
             use! res = req.AsyncGetResponse() 
             use stream = res.GetResponseStream()
             use reader = new StreamReader(stream)
-            let! rdata = reader.ReadToEndAsync() |> Async.AwaitTask        
-            return rdata
+            let responseText = reader.ReadToEnd()       
+            return responseText
         }
--- Fable.Remoting.DotnetClient/Proxy.fs
+++ Fable.Remoting.DotnetClient/Proxy.fs
@@ -7,7 +7,6 @@ open Newtonsoft.Json
 module Proxy =
     
     open Patterns
-
     let private converter = FableJsonConverter()
 
     /// Parses a JSON iput string to a .NET type using Fable JSON converter
@@ -15,15 +14,26 @@ module Proxy =
         JsonConvert.DeserializeObject<'t>(json, converter)
     
     /// Sends a POST request to the specified url with the arguments of serialized to an input list
-    let proxyPost<'t> (functionArguments: obj list) url = 
+    let proxyPost<'t> (functionArguments: obj list) url auth = 
         let serializedInputArgs = JsonConvert.SerializeObject(functionArguments, converter)
         async {
-            let! responseText = Http.makePostRequest url serializedInputArgs
+            let! responseText = Http.makePostRequest url serializedInputArgs auth
             return parseAs<'t> responseText
         }
         
     type Proxy<'t>(builder) = 
         let typeName = typeof<'t>.Name
+        let mutable authHeader = Http.Authorisation.NoToken
+        
+        /// Adds the specified string as the authorization header for the requests that the proxy makes to the server
+        member this.authorisationHeader (header: string) = 
+            authHeader <- Http.Authorisation.Token header
+
+        /// Call the proxy function by wrapping it inside a quotation expr:
+        /// `async { 
+        ///     let proxy = Proxy.create<IServer> (sprintf "http://api.endpoint.org/api/%s/%s")  
+        ///     let! result = proxy.call <@ server -> server.getLength "input" @>
+        ///  }  `    
         member this.call<'u> (expr: Quotations.Expr<'t -> Async<'u>>) : Async<'u> = 
             match expr with 
             | NoArgs (methodName, args) 
@@ -36,7 +46,6 @@ module Proxy =
             | SevenArgs (methodName, args)
             | EightArgs (methodName, args) -> 
                 let route = builder typeName methodName
-                proxyPost<'u> args route
-            | otherwise -> failwithf "Quatation expression %A cannot be processed" expr;
-   
+                proxyPost<'u> args route authHeader
+            | otherwise -> failwithf "Quatation expression %A cannot be processed" expr
     let create<'t> builder = Proxy<'t>(builder)
