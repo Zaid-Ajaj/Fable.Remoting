6bbb63829c914b1a624156d93b0b030710d6f3d5
Zaid <zaid.naom@gmail.com>
2020-09-26 22:38:01 +0200

    Fable.Remoting.Client v6.12 with file juggling extensions for easier upload and download

--- Fable.Remoting.ClientV2/Proxy.fs
+++ Fable.Remoting.ClientV2/Proxy.fs
@@ -10,9 +10,6 @@ module internal Blob =
     /// Creates a Blob from the given input string
     [<Emit("new Blob([$0.buffer], { type: 'text/plain' })")>]
     let fromBinaryEncodedText (value: byte[]) : Blob = jsNative
-    
-    [<Emit("new FileReader()")>]
-    let createFileReader() : FileReader = jsNative
 
     /// Asynchronously reads the blob data content as string
     let readBlobAsText (blob: Blob) : Async<string> =
@@ -34,14 +31,14 @@ module Proxy =
 
     [<Emit("$0[$1]")>]
     let private getAs<'a> (x: obj) (key: string) : 'a = jsNative
-    
+
     [<Emit("JSON.parse($0)")>]
     let private jsonParse (content: string) : obj = jsNative
-    
+
     [<Emit("JSON.stringify($0)")>]
     let private stringify (x: obj) : string = jsNative
-    
-    let combineRouteWithBaseUrl route (baseUrl: string option) = 
+
+    let combineRouteWithBaseUrl route (baseUrl: string option) =
         match baseUrl with
         | None -> route
         | Some url -> sprintf "%s%s" (url.TrimEnd('/')) route
@@ -50,70 +47,70 @@ module Proxy =
     let arguments() : obj[] = jsNative
 
     /// Extracts the 'T from Async<'T>
-    let extractAsyncArg (asyncType: Type) = 
+    let extractAsyncArg (asyncType: Type) =
         asyncType.GenericTypeArguments.[0]
 
-    let isByteArray = function 
+    let isByteArray = function
         | TypeInfo.Array getElemType ->
-            match getElemType() with 
-            | TypeInfo.Byte -> true 
-            | otherwise -> false 
-        | otherwise -> false 
+            match getElemType() with
+            | TypeInfo.Byte -> true
+            | otherwise -> false
+        | otherwise -> false
 
-    let isAsyncOfByteArray = function 
-        | TypeInfo.Async getAsyncType -> 
-            match getAsyncType() with 
+    let isAsyncOfByteArray = function
+        | TypeInfo.Async getAsyncType ->
+            match getAsyncType() with
             | TypeInfo.Array getElemType ->
-                match getElemType() with 
-                | TypeInfo.Byte -> true 
-                | otherwise -> false 
-            | otherwise -> false 
+                match getElemType() with
+                | TypeInfo.Byte -> true
+                | otherwise -> false
+            | otherwise -> false
         | otherwise -> false
 
     let proxyFetch options typeName (func: RecordField) fieldType =
-        let funcArgs : (TypeInfo [ ]) = 
-            match func.FieldType with  
+        let funcArgs : (TypeInfo [ ]) =
+            match func.FieldType with
             | TypeInfo.Async inner -> [| func.FieldType |]
             | TypeInfo.Promise inner -> [| func.FieldType |]
-            | TypeInfo.Func getArgs -> getArgs() 
+            | TypeInfo.Func getArgs -> getArgs()
             | _ -> failwithf "Field %s does not have a valid definiton" func.FieldName
 
         let argumentCount = (Array.length funcArgs) - 1
         let returnTypeAsync = Array.last funcArgs
-        let returnType = 
-            match returnTypeAsync with 
+        let returnType =
+            match returnTypeAsync with
             | TypeInfo.Async getAsyncTypeArgument -> getAsyncTypeArgument()
             | TypeInfo.Promise getPromiseTypeArgument -> getPromiseTypeArgument()
             | _ -> failwithf "Expected field %s to have a return type of Async<'t>" func.FieldName
 
-        let readAsBinary = isAsyncOfByteArray returnTypeAsync 
-        
-        let binaryInput = 
-            match func.FieldType with 
-            | TypeInfo.Func getArgs -> 
-                match getArgs() with 
-                | [| input; output |] -> isByteArray input 
-                | otherwise -> false 
-            | otherwise -> false 
-        
+        let readAsBinary = isAsyncOfByteArray returnTypeAsync
+
+        let binaryInput =
+            match func.FieldType with
+            | TypeInfo.Func getArgs ->
+                match getArgs() with
+                | [| input; output |] -> isByteArray input
+                | otherwise -> false
+            | otherwise -> false
+
         let route = options.RouteBuilder typeName func.FieldName
-        let url = combineRouteWithBaseUrl route options.BaseUrl 
-        let funcNeedParameters = 
-            match funcArgs with  
-            | [| TypeInfo.Async _ |] -> false 
+        let url = combineRouteWithBaseUrl route options.BaseUrl
+        let funcNeedParameters =
+            match funcArgs with
+            | [| TypeInfo.Async _ |] -> false
             | [| TypeInfo.Promise _ |] -> false
-            | [| TypeInfo.Unit; TypeInfo.Async _ |] -> false 
-            | otherwise -> true 
+            | [| TypeInfo.Unit; TypeInfo.Async _ |] -> false
+            | otherwise -> true
 
         fun arg0 arg1 arg2 arg3 arg4 arg5 arg6 arg7 ->
 
             let inputArguments =
-               if funcNeedParameters  
+               if funcNeedParameters
                then List.take argumentCount [ box arg0;box arg1;box arg2;box arg3;box arg4;box arg5;box arg6;box arg7 ]
                else [ ]
-                        
-            let contentType = 
-                if binaryInput 
+
+            let contentType =
+                if binaryInput
                 then "application/octet-stream"
                 else "application/json; charset=utf8"
 
@@ -122,31 +119,31 @@ module Proxy =
                   [ yield "Content-Type", contentType
                     yield "x-remoting-proxy", "true"
                     yield! options.CustomHeaders
-                    match options.Authorization with 
+                    match options.Authorization with
                     | Some authToken -> yield "Authorization", authToken
                     | None -> () ]
 
-                let requestBody = 
-                    if binaryInput 
+                let requestBody =
+                    if binaryInput
                     then RequestBody.Binary (unbox arg0)
                     else RequestBody.Json (Json.stringify inputArguments)
 
                 match options.ResponseSerialization with
                 | MessagePack ->
                     // read as arraybuffer and deserialize
-                    let! (response, statusCode) = 
-                        if funcNeedParameters 
-                        then 
+                    let! (response, statusCode) =
+                        if funcNeedParameters
+                        then
                             Http.post url
                             |> Http.withBody requestBody
-                            |> Http.withHeaders headers 
+                            |> Http.withHeaders headers
                             |> Http.sendAndReadBinary
-                        else 
-                            Http.get url 
-                            |> Http.withHeaders headers  
+                        else
+                            Http.get url
+                            |> Http.withHeaders headers
                             |> Http.sendAndReadBinary
-                    
-                    match statusCode with 
+
+                    match statusCode with
                     | 200 ->
                         let rec getReturnType typ =
                             if Reflection.FSharpType.IsFunction typ then
@@ -156,65 +153,65 @@ module Proxy =
                                 typ.GetGenericArguments () |> Array.head
                             else
                                 typ
-                        
+
                         return MsgPack.Read.Reader(response).Read (getReturnType fieldType)
                     | 500 ->
                         let responseAsBlob = Blob.fromBinaryEncodedText response
                         let! responseText = Blob.readBlobAsText responseAsBlob
                         let response = { StatusCode = statusCode; ResponseBody = responseText }
-                        return! raise (ProxyRequestException(response, sprintf "Internal server error (500) while making request to %s" url, response.ResponseBody)) 
+                        return! raise (ProxyRequestException(response, sprintf "Internal server error (500) while making request to %s" url, response.ResponseBody))
                     | n ->
                         let responseAsBlob = Blob.fromBinaryEncodedText response
                         let! responseText = Blob.readBlobAsText responseAsBlob
                         let response = { StatusCode = statusCode; ResponseBody = responseText }
                         return! raise (ProxyRequestException(response, sprintf "Http error (%d) while making request to %s" n url, response.ResponseBody))
                 | Json ->
-                    match readAsBinary with 
-                    | true -> 
+                    match readAsBinary with
+                    | true ->
                         // don't deserialize, read as arraybuffer and convert to byte[]
-                        let! (response, statusCode) = 
-                            if funcNeedParameters 
-                            then 
+                        let! (response, statusCode) =
+                            if funcNeedParameters
+                            then
                                 Http.post url
                                 |> Http.withBody requestBody
-                                |> Http.withHeaders headers 
+                                |> Http.withHeaders headers
                                 |> Http.sendAndReadBinary
-                            else 
-                                Http.get url 
-                                |> Http.withHeaders headers  
+                            else
+                                Http.get url
+                                |> Http.withHeaders headers
                                 |> Http.sendAndReadBinary
-                        
-                        match statusCode with 
-                        | 200 -> 
-                            return unbox response 
+
+                        match statusCode with
+                        | 200 ->
+                            return unbox response
                         | 500 ->
                             let responseAsBlob = Blob.fromBinaryEncodedText response
                             let! responseText = Blob.readBlobAsText responseAsBlob
                             let response = { StatusCode = statusCode; ResponseBody = responseText }
-                            return! raise (ProxyRequestException(response, sprintf "Internal server error (500) while making request to %s" url, response.ResponseBody)) 
+                            return! raise (ProxyRequestException(response, sprintf "Internal server error (500) while making request to %s" url, response.ResponseBody))
                         | n ->
                             let responseAsBlob = Blob.fromBinaryEncodedText response
                             let! responseText = Blob.readBlobAsText responseAsBlob
                             let response = { StatusCode = statusCode; ResponseBody = responseText }
-                            return! raise (ProxyRequestException(response, sprintf "Http error (%d) while making request to %s" n url, response.ResponseBody)) 
+                            return! raise (ProxyRequestException(response, sprintf "Http error (%d) while making request to %s" n url, response.ResponseBody))
                     | false ->
                         // make plain RPC request and let it go through the deserialization pipeline
-                        let! response = 
-                            if funcNeedParameters 
-                            then 
+                        let! response =
+                            if funcNeedParameters
+                            then
                                 Http.post url
                                 |> Http.withBody requestBody
-                                |> Http.withHeaders headers 
-                                |> Http.send 
-                            else  
-                                Http.get url 
-                                |> Http.withHeaders headers  
-                                |> Http.send 
+                                |> Http.withHeaders headers
+                                |> Http.send
+                            else
+                                Http.get url
+                                |> Http.withHeaders headers
+                                |> Http.send
 
-                        match response.StatusCode with  
-                        | 200 -> 
+                        match response.StatusCode with
+                        | 200 ->
                             let parsedJson = SimpleJson.parseNative response.ResponseBody
-                            return Convert.fromJsonAs parsedJson returnType  
-                        | 500 -> return! raise (ProxyRequestException(response, sprintf "Internal server error (500) while making request to %s" url, response.ResponseBody)) 
-                        | n ->   return! raise (ProxyRequestException(response, sprintf "Http error (%d) from server occured while making request to %s" n url, response.ResponseBody)) 
+                            return Convert.fromJsonAs parsedJson returnType
+                        | 500 -> return! raise (ProxyRequestException(response, sprintf "Internal server error (500) while making request to %s" url, response.ResponseBody))
+                        | n ->   return! raise (ProxyRequestException(response, sprintf "Http error (%d) from server occured while making request to %s" n url, response.ResponseBody))
             }
